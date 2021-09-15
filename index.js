1f8e1ccfb5e3489d184a934f3f40232c80726500
Garrett <garres4@vt.edu>
2021-09-15 10:19:06 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const thankYou = [];
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        thankYou.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        debugger;
+    }
+    return thankYou;
+}
+
+function countDown() {
+    let i = 10;
+    while (i <= 10 && i >=0) {
+        console.log(i--);
+    }
+}
